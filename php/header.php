da4efa00cf1399c6ad64e984c4892f5c9f32d158
FSSA Dev <jdmagee93@gmail.com>
2016-03-02 21:57:15 -0400

    Worked on site banner.

---
+++ php/header.php
@@ -0,0 +1,24 @@
+<?php
+?>
+
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="../css/header.css"/>
+	<link rel="stylesheet" href="../js/jquery-ui-1.11.4.custom/jquery-ui.min.css">
+    <script src="../js/jquery-ui-1.11.4.custom/external/jquery/jquery.js"></script>
+	<script src="../js/jquery-ui-1.11.4.custom/jquery-ui.min.js"></script>
+</head>
+<body>
+	<div class="header">
+		<img src="../images/FSSA_Crest_with_text.jpg" alt="FSSA Crest"/>
+	</div>
+	<div class="corner_login">
+		<form action="" method="post" id="corner_login">
+			Username: <input type="text" name="username"><br>
+			Password: <input type="password" name="password"><br>
+			<input type="submit" value="Login">
+		</form>
+		<h3>Sign up!</h3>
+	</div>
+</body>
+</html>
--- php/index.php
+++ php/index.php
@@ -1,3 +1,18 @@
 <?php
-echo "Hello World! It's me, Mario"
+
+if(isset($_POST['username'])){
+    session_start();
+    $_SESSION['name']=$_POST['username'];
+    
+}
+include("../php/header.php");
     ?>
+    
+<html>
+	<head>
+		<link rel="stylesheet" href="../js/jquery-ui-1.11.4.custom/jquery-ui.min.css">
+    	<script src="../js/jquery-ui-1.11.4.custom/external/jquery/jquery.js"></script>
+		<script src="../js/jquery-ui-1.11.4.custom/jquery-ui.min.js"></script>
+		<title>Fredericton Society of Saint Andrew</title>
+	</head>
+</html>
