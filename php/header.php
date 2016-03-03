d5be6b63b96503c11208b5a97943506002f68836
FSSA Dev <jdmagee93@gmail.com>
2016-03-03 10:47:13 -0400

    Removed js folder.

--- php/header.php
+++ php/header.php
@@ -4,9 +4,6 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="../css/header.css"/>
-	<link rel="stylesheet" href="../js/jquery-ui-1.11.4.custom/jquery-ui.min.css">
-    <script src="../js/jquery-ui-1.11.4.custom/external/jquery/jquery.js"></script>
-	<script src="../js/jquery-ui-1.11.4.custom/jquery-ui.min.js"></script>
 </head>
 <body>
 	<div class="header">
--- php/index.php
+++ php/index.php
@@ -10,9 +10,6 @@ include("../php/header.php");
     
 <html>
 	<head>
-		<link rel="stylesheet" href="../js/jquery-ui-1.11.4.custom/jquery-ui.min.css">
-    	<script src="../js/jquery-ui-1.11.4.custom/external/jquery/jquery.js"></script>
-		<script src="../js/jquery-ui-1.11.4.custom/jquery-ui.min.js"></script>
 		<title>Fredericton Society of Saint Andrew</title>
 	</head>
 </html>
