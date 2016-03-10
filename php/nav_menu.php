e391ca46384aaf9d9eb7d22256bbc922cea2ff9f
FSSA Dev <jdmagee93@gmail.com>
2016-03-10 16:26:01 -0400

    Created php file that will be navigation menu

---
+++ php/nav_menu.php
@@ -0,0 +1,21 @@
+<?php
+?>
+
+<html>
+<head>
+	<script type="text/javascript" src="jquery-1.12.1.min.js"></script>
+</head>
+<body>
+	<div class="loggedOutMenu">
+		<a href="index.php">FSSA Information</a>
+		<br>
+		<a href="index.php">Research Heritage</a>
+		<br>
+		<a href="index.php">Upcoming Events</a>
+		<br>
+		<a href="index.php">Join Us!</a>
+		<br>
+		<a href="index.php">Members Only</a>
+	</div>
+</body>
+</html>
