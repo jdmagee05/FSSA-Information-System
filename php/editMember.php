5fcba1f1ca2f89b8a92caee0d251c7276df1585b
Drew Murray <Drew Murray>
2016-03-21 22:05:00 -0300

    Add to 'andrew'
    # member.css
    # addMember.php
    # editMember.php
    
    Signed-off-by: Drew Murray <Drew Murray>

--- php/editMember.php
+++ php/editMember.php
@@ -9,31 +9,30 @@ if(isset($_POST['username'])){
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="../css/header.css"/>
+	<link rel="stylesheet" type="text/css" href="../css/member.css"/>
 </head>
 <body>
-	<div class="header">
-		<img src="../images/FSSA_Crest_with_text.jpg" alt="FSSA Crest"/>
-	</div>
 	<?php if(!isset($_POST['username'])){ ?>
-	<div class="welcome_member"><h3>Welcome<br>Member Name!</h3></div>
-     <div class="edit_member">
-     <h3>Edit information for: Member Name</h3>
-		<form action="" method="post" id="edit_member">
-			First Name: <input type="text" name="firstname"><br>
-			Last Name: <input type="text" name="lastname"><br>
-			Middle Name: <input type="text" name="middlename"><br>
-			Birth Date: <input type="date" name="birthdate"><br>
-			Birth Place: <input type="text" name="birthplace"><br><br>
-			Address: <input type="text" name="address"><br>
-			City: <input type="text" name="city"><br>
-			Province: <input type="text" name="province"><br>
-			Country: <input type="text" name="country"><br>
-			Postal Code: <input type="text" name="postalcode"><br>
-			
-			Home Phone: <input type="text" name="homephone"><br>
-			Cell Phone: <input type="text" name="cellphone"><br>
-			Email: <input type="text" name="email"><br><br>
-			Member Type: <select name="membertype">
+	<form action="" method="post" id="edit_member">
+     <div id="wrapper">
+     <h3 align="center">Edit information for: Member Name</h3>
+     	<div id="first">
+			<label>First Name:</label><input type="text" name="firstname"><br>
+			<label>Last Name:</label><input type="text" name="lastname"><br>
+			<label>Middle Name:</label><input type="text" name="middlename"><br>
+			<label>Birth Date:</label><input type="date" name="birthdate"><br>
+			<label>Birth Place:</label><input type="text" name="birthplace"><br><br>
+			<label>Address:</label><input type="text" name="address"><br>
+			<label>City:</label><input type="text" name="city"><br>
+			<label>Province:</label><input type="text" name="province"><br>
+			<label>Country:</label><input type="text" name="country"><br>
+			<label>Postal Code:</label><input type="text" name="postalcode"><br>
+		</div>
+		<div id="second">
+			<label>Home Phone:</label><input type="text" name="homephone"><br>
+			<label>Cell Phone:</label><input type="text" name="cellphone"><br>
+			<label>Email:</label><input type="text" name="email"><br><br>
+			<label>Member Type:</label><select name="membertype">
 			<option value="regular">Regular</option>
 			<option value="associate">Associate</option>
 			<option value="5-year">5-Year</option>
@@ -41,17 +40,20 @@ if(isset($_POST['username'])){
 			<option value="honorarylife">Honorary Life</option>
 			<option value="historical">Historical</option>
 			</select><br>
-			Member Status: <select name="memberstatus">
+			<label>Member Status:</label><select name="memberstatus">
 			<option value="normal">Normal</option>
 			<option value="deceased">Deceased</option>
 			<option value="lapsed">Lapsed</option>
 			<option value="moved">Moved</option>
 			</select><br>
-			Membership Expiry Date: dd/mm/yyyy<br>
+			<label>Membership Expiry Date:</label> dd/mm/yyyy<br>
+		</div>
+		</div>
+		<div align="center">
 			<input type="submit" value="Submit">
 			<input type="reset" value="Clear">
+		</div>
 		</form>
-	</div>
 	<?php } ?>
 	
 </body>
